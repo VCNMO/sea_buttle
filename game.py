e05e840be4c82e9bcc28029325cf211f93cf9a00
Makarenkomd <mahadrum@yandex.ru>
2023-04-17 11:38:43 +0300

    Добавлено автоматическое движение корабля
    Не срабатывают клавиши управления(

--- game.py
+++ game.py
@@ -3,6 +3,21 @@ v = 10
 x = 0
 y = 3 
 y_boom = 300
+boom = False
+
+def animation():
+    global x, v
+    x += v
+    if boom:
+        y_boom -= 5
+        draw_game.coords(image_boom, 300, y_boom)    
+    if x > 600:
+        v = -v
+    if x < -100:
+        v = -v
+    draw_game.coords(image_ship, x, y)
+    draw_game.after(20, animation)
+    
 def move_ship_right(even):
     global x
     x += v
@@ -14,10 +29,9 @@ def move_ship_left(even):
     draw_game.coords(image_ship, x, y)    
 
 def move_ship_up(even):
-    global y_boom
-    y_boom -= 5
-    draw_game.coords(image_boom, 300, y_boom)    
-
+    global boom
+    boom = not boom
+    print(boom)
 
 win = tk.Tk()
 draw_game = tk.Canvas(win, width=600, height=400, bg="#40B3E0")
@@ -30,8 +44,9 @@ pic_boom = tk.PhotoImage(file="boom2.png")
 image_sea = draw_game.create_image(0, 100, anchor=tk.NW, image=pic_sea) 
 image_ship = draw_game.create_image(x, y, anchor=tk.NW, image=pic_ship)
 image_boom = draw_game.create_image(300, 300, anchor=tk.NW, image=pic_boom)
-win.bind('<Right>', move_ship_right)
-win.bind('<Left>', move_ship_left)
-win.bind('<Up>', move_ship_up)
+#win.bind('<Right>', move_ship_right)
+#win.bind('<Left>', move_ship_left)
+#win.bind('<Up>', move_ship_up)
+animation()
 
 win.mainloop()
